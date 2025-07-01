e361e7aea867534ec80604ca8dac85072c883918
Allan K. Koech <allankoech.ke@gmail.com>
2025-07-01 23:53:59 +0300

    Dropped redundant files + added favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'Mantis Admin',
-  description: 'Admin dashboard for Mantis BaaS',
-  generator: 'mantis.app',
+  title: 'Mantis Admin Dashboard',
+  description: 'Admin dashboard for Mantis: A lightweight BaaS in C++'
 }
 
 export default function RootLayout({
@@ -14,6 +13,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body>{children}</body>
     </html>
   )
